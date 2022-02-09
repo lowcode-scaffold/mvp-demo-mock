90585dfd3d3e8eb5ae2d70f0ed1540c09c269906
吴俊康 <wujk03@vanke.com>
2022-02-09 09:46:49 +0800

    update

--- src/routes/mvp-demo.js
+++ src/routes/mvp-demo.js
@@ -8,27 +8,44 @@ const Mock = require('mockjs');
 const { Random } = Mock;
 
 const router = new KoaRouter();
-router.get(`/api/user/page`, async (ctx, next) => {
-  const list1 = [];
-  for (let i = 0; i < 10; i++) {
-    const list2 = [];
-    for (let j = 0; j < 10; j++) {
-      list2.push(Random.cword(5, 6));
+router
+  .get(`/api/user/page`, async (ctx, next) => {
+    const list1 = [];
+    for (let i = 0; i < 10; i++) {
+      const list2 = [];
+      for (let j = 0; j < 10; j++) {
+        list2.push(Random.cword(5, 6));
+      }
+      list1.push({
+        name: Random.cword(5, 6),
+        age: Random.natural(1, 100),
+        mobile: Random.natural(),
+        address: Random.cword(20, 30),
+        tags: list2,
+        id: Random.natural(100, 1000),
+      });
     }
-    list1.push({
-      name: Random.cword(5, 6),
-      age: Random.natural(1, 100),
-      mobile: Random.natural(),
-      address: Random.cword(20, 30),
-      tags: list2,
-      id: Random.natural(100, 1000),
-    });
-  }
-  // delay(2);
-  ctx.body = {
-    code: 200,
-    msg: Random.cword(5, 6),
-    result: { rows: list1, total: 200 },
-  };
-});
+    // delay(2);
+    ctx.body = {
+      code: 200,
+      msg: Random.cword(5, 6),
+      result: { rows: list1, total: 200 },
+    };
+  })
+  .post(`/api/user/create`, async (ctx, next) => {
+    delay(2);
+    ctx.body = {
+      code: 200,
+      msg: Random.cword(5, 6),
+      result: Random.natural(100, 1000),
+    };
+  })
+  .post(`/api/user/edit`, async (ctx, next) => {
+    delay(2);
+    ctx.body = { code: 200, msg: Random.cword(5, 6), result: Random.boolean() };
+  })
+  .delete(`/api/user/del`, async (ctx, next) => {
+    delay(2);
+    ctx.body = { code: 200, msg: Random.cword(5, 6), result: Random.boolean() };
+  });
 module.exports = router;
