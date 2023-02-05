ec83cc759eeca151a9d98f773d1060da05de5c93
wjkang <ruoxieme@gmail.com>
2023-02-06 00:38:23 +0800

    update

--- src/routes/rbac.js
+++ src/routes/rbac.js
@@ -5,19 +5,21 @@ import { delay } from '../lib/util';
 
 const router = new KoaRouter();
 router.get(`/application/page`, async (ctx, next) => {
+  const { size } = ctx.query;
   const list1 = [];
-  for (let i = 0; i < 10; i++) {
+  delay(1);
+  for (let i = 0; i < size; i++) {
     list1.push({
       name: Random.cword(5, 6),
-      code: Random.cword(5, 6),
-      createdDate: Random.cword(5, 6),
-      updatedDate: Random.cword(5, 6),
+      code: Random.word(5, 6),
+      createdDate: Random.datetime(),
+      updatedDate: Random.datetime(),
     });
   }
   ctx.body = {
     code: 200,
     msg: '',
-    result: list1,
+    result: { records: list1, pages: 20 },
   };
 });
 module.exports = router;
